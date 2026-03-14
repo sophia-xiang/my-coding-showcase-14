ca5ed4eae71aadb193176134fe99f974cd9f6667
sophia-xiang <6176023227so@gmail.com>
2026-03-14 10:26:53 -0400

    Remove basename from HashRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/my-coding-showcase-14/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
